333a7c1d860fe555c1599b529709bc14b996c6e5
Ridhvi-max <ridhvishringi13@gmail.com>
2025-05-29 16:03:48 +0530

    Add files via upload

---
+++ lesson3.py
@@ -0,0 +1,12 @@
+name = "penguin"
+age = 15
+is_student=True
+weight = 38.5
+print("Name :", name)
+print("Data type of name is", type(name))
+print("Age :", age)
+print("Data type Age is", type(age))
+print("is_student :", is_student)
+print("Data type of is_student is", type(is_student))
+print("Weigth :", weight)
+print("Data type of weigth is", type(weight))
