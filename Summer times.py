50f5ec5bcfbe1c48b7a0df23eed888a7d5cbd7f2
Ridhvi-max <ridhvishringi13@gmail.com>
2025-06-02 12:57:41 +0530

    Add files via upload

---
+++ Summer times.py
@@ -0,0 +1,7 @@
+temp = float(input("Please write the temperature of your area: "))
+if temp> 20:
+    print("You can wear light and soft clothes")
+elif temp<20:
+    print("You should wear warm clothes")
+else:
+    print("If you are felling cold you can wear winter clothes otherwise you can wear light and soft clothes it is your choice")
