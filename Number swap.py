0284953095a78da3a4ce8605a75b05af96aa8e18
Ridhvi-max <ridhvishringi13@gmail.com>
2025-06-04 13:18:28 +0530

    Add files via upload

---
+++ Number swap.py
@@ -0,0 +1,9 @@
+x=36
+y=29
+z=11
+#All the three numbers are swaped thus the numbers now , x should be y z should be x and y should be z
+print("Wrong order: ")
+print("x=", x,"y=", y,"z=", z)
+x,y,z=y,z,x,
+print("\nCorrect order:")
+print("x=",y,"y=",z,"z=",x)
