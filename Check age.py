1c3e373053018d928b400e04ec968a92d9100f25
Ridhvi-max <ridhvishringi13@gmail.com>
2025-06-05 13:19:40 +0530

    Add files via upload

---
+++ Check age.py
@@ -0,0 +1,7 @@
+Age=int(input("Enter your age: "))
+if Age<20:
+    print("You have access to enroll in the class!")
+elif Age>20:
+    print("You do not have access to enrol in the class.")
+else:
+    print("You have access to enroll in the class!")
