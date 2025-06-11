c26841fbbd9198df2470ce613708afcb6a373733
Ridhvi-max <ridhvishringi13@gmail.com>
2025-06-11 11:32:05 +0530

    Add files via upload

---
+++ Circumference.py
@@ -0,0 +1,6 @@
+def circumference(r):
+    pie=22/7
+    return 2*pie*r
+print("Hello this is a program to calculate circumference of a circle")
+radius=int(input("Please type the radius of the circle: "))
+print("Circumference of the circle with Radius" ,radius ,"is:" ,circumference(radius))
