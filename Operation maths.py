e5157081773eb1182694899fdce76e80dc90a4c1
Ridhvi-max <ridhvishringi13@gmail.com>
2025-06-14 15:09:05 +0530

    Add files via upload

---
+++ Operation maths.py
@@ -0,0 +1,13 @@
+import math
+print(math.sin(45))
+print(math.cos(45))
+print(math.tan(45))
+print(math.ceil(45,490))
+print(math.floor(45.890))
+print(math.factorial(5))
+print(math.exp(5))
+print(math.fabs(45.69))
+x=10
+y=-5
+print(math.copysign(x,y))
+print(math.gcd(18,24))
