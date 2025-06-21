22ac0f9f3e1fa03934fec9c2a41df1e131db4945
Ridhvi-max <ridhvishringi13@gmail.com>
2025-06-21 09:42:53 +0530

    Add files via upload

---
+++ Cheack the fruits frequency.py
@@ -0,0 +1,8 @@
+text={"Apples":20,"orange":10,"banana":15,"mango":10,"Grapes":10,}
+print("The orignal dictionary:"+str(text))
+check=10
+friut=0
+for key in friut:
+    if text[key]==check:
+        fruit=fruit
+print("Frequency of 10 in fruit is :"+str(Fruit))
